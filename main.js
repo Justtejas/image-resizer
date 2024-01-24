c8f80ec5304437e023e705032cd293dae93c24fc
Justtejas <tejaspantoji@gmail.com>
2024-01-24 16:11:26 +0530

    feat: main & about window and styles, added assests

--- main.js
+++ main.js
@@ -1,20 +1,87 @@
 const path = require("path");
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu} = require("electron");
 
+const isMac = process.platform === "darwin";
+const isDev = process.env.NODE_ENV !== "production";
+
+// create main window
 function createMainWindow() {
-	const mainWindow = new BrowserWindow({
-		title: "Image Resizer",
-		width: 500,
-		height: 600,
-	});
-	mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
+  const mainWindow = new BrowserWindow({
+    title: "Image Resizer",
+    width: isDev ? 1000 : 500,
+    height: 600,
+  });
+
+  // open devtools if in development mode
+  if (isDev) {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.loadFile(path.join(__dirname, "./renderer/index.html"));
 }
 
-app.whenReady().then(createMainWindow);
+//create about window
+function createAboutWindow() {
+  const aboutWindow = new BrowserWindow({
+    title: "About Image Resizer",
+    width: 300,
+    height: 300,
+    resizable: false,
+  });
+  aboutWindow.setMenuBarVisibility(false);
+  aboutWindow.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
 
-app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
+// app is ready
+app.whenReady().then(() => {
+  createMainWindow();
+
+  // implement menu
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createMainWindow();
+    }
+  });
 });
 
+// Menu template
+const menu = [
+  ...(isMac
+    ? [
+        {
+          label: app.name,
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+  {
+    role: "fileMenu",
+  },
+  ...(!isMac
+    ? [
+        {
+          label: "Help",
+          submenu: [
+            {
+              label: "About",
+              click: createAboutWindow,
+            },
+          ],
+        },
+      ]
+    : []),
+];
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
